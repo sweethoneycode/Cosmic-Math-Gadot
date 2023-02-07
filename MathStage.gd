8cd6e0e3aaaab985ef7568997a2e1e9d8498edbc
Jonah Brown <support@sweethoneycode.com>
2023-02-07 08:46:54 -0800

    math stage updates

--- MathStage.gd
+++ MathStage.gd
@@ -1,10 +1,6 @@
 extends Node2D
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
 var wrongAnswers: int = 0
 onready var symbolTxt := $"%Symbol"
 onready var addend1Txt := $"%Num1"
@@ -15,24 +11,49 @@ var minNum = 0
 var maxNum = 0
 export var Addend1num: int = 0
 var Addend2num: int = 0
-var mathType = "/"
-
+export var mathType = "/"
+var number: = 0
+var random = RandomNumberGenerator.new()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	
 	if(mathType == "/"):
 		mathType="÷"
+		setDivideNum()
+	else:
+		setSecondNum()	
 		
 	symbolTxt.text = str(mathType)
 	
 	addend1Txt.text = str(Addend1num)
-	Addend1num = 12
-	setSecondNum()
-
+	Addend1num = 1
+	
 	
 func createQuestions():
 	pass
+
+func setDivideNum():
+	print("Divide")
+	if(Addend1num < 11):
+		minNum = 1
+		maxNum = 1
+	else:
+		minNum = 0
+		maxNum = 21
+		
+	for _i in range(10):
+		#random.randomize()
+		number = Addend1num
+		while Addend2.has(number):
+			var num = random.randi_range(1, 12)
+			number = num * Addend1num
+		Addend2.append(number)
 	
+	Addend2.shuffle()
+	print(Addend2)
+	addend2Txt.text = str(Addend2[0])
+
 func setSecondNum():
 	
 	if(Addend1num < 10):
@@ -41,37 +62,15 @@ func setSecondNum():
 	else:
 		minNum = 0
 		maxNum = 20
-	
-	if(mathType == "/"):
-		if(Addend1num < 11):
-			minNum = 0
-			maxNum = 10
-		else:
-			minNum = 0
-			maxNum = 21
-	
-	var number: = 0
-	var random = RandomNumberGenerator.new()
-	
-	if(mathType != "/"):
-		for i in range(10):
-			random.randomize()
+		
+	for _i in range(10):
+		number = Addend1num
+		while Addend2.has(number):
+			number = random.randi_range(minNum, maxNum)
+
+		Addend2.append(number)
 			
-			while Addend2.has(number):
-				number = random.randi_range(minNum, maxNum)
-			
-			Addend2.append(number)
-	else:
-		for i in range(10):
-			random.randomize()
-			
-			while Addend2.has(number):
-				var num = random.randi_range(1, 12)
-				number = num * Addend1num
-			
-			Addend2.append(number)
-	
-	#Addend2.shuffle()
+	Addend2.shuffle()
 	print(Addend2)
 	addend2Txt.text = str(Addend2[0])
 
