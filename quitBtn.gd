e9d7998e87138108614477b2797c1bb0323275e8
Jonah Brown <support@sweethoneycode.com>
2023-10-07 23:40:00 -0700

    Updates

--- quitBtn.gd
+++ quitBtn.gd
@@ -1,14 +1,9 @@
 extends Button
 
 var _save: SaveGame
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
 
-onready var DefaultIMG := $"%Default"
-onready var MainMenu := $"%Menu"
-onready var SettingsMenu := $"../Menu/SettingsBtn"
-
+onready var DefaultIMG := $"%bkgrnd"
+onready var SettingsMenu := $"../SettingsBtn"
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	_create_or_load_save() #load saved game
