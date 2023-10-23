f2b9729ffe3dcd2919bb0f19492781264d465bc0
Jonah Brown <support@sweethoneycode.com>
2023-10-22 21:23:49 -0700

    Update levelBtn.gd

--- Scenes/levelBtn.gd
+++ Scenes/levelBtn.gd
@@ -23,45 +23,32 @@ func checkStage():
 	
 	match mathType:
 		"+":
-			if(!_save.AdditionComplete.empty()):
-				if(_save.AdditionUnlock.get(currentLevel) == 1):
-					var stars = _save.AdditionComplete.get(currentLevel)
-					for star in $stars.get_children():
-						if(int(star.name) <= stars):
-							star.show()
-				else:
-					self.disabled = true
+			if(_save.AdditionUnlock.get(currentLevel) == 1):
+				var stars = _save.AdditionComplete.get(currentLevel)
+				loadStars(stars)
+			else:
+				self.disabled = true
 			currMath = "addition"
 		"-":
-			if(!_save.SubtractionComplete.empty()):
-				if(_save.SubtractionComplete.get(currentLevel) == 1):
-					var stars = _save.SubtractionComplete.get(currentLevel)
-					for star in $stars.get_children():
-						if(int(star.name) <= stars):
-							star.show()
-				else:
-					self.disabled = true
+			if(_save.SubtractionComplete.get(currentLevel) == 1):
+				var stars = _save.SubtractionComplete.get(currentLevel)
+				loadStars(stars)
+			else:
+				self.disabled = true
 			currMath = "subtraction"
 		"x":
-			if(!_save.MultiComplete.empty()):
-				if(_save.MultiplicationUnlock.get(currentLevel) == 1):
-					var stars = _save.MultiComplete.get(currentLevel)
-					for star in $stars.get_children():
-						if(int(star.name) <= stars):
-							star.show()
-				else:
-					self.disabled = true
+			if(_save.MultiplicationUnlock.get(currentLevel) == 1):
+				var stars = _save.MultiComplete.get(currentLevel)
+				loadStars(stars)
+			else:
+				self.disabled = true
 			currMath = "multiplication"
 		"÷":
-			if(!_save.DivisionComplete.empty()):
-				if(_save.DivisionUnlock.get(currentLevel) == 1):
-					var stars = _save.DivisionComplete.get(currentLevel)
-					print(stars)
-					for star in $stars.get_children():
-						if(int(star.name) <= stars):
-							star.show()
-				else:
-					self.disabled = true
+			if(_save.DivisionUnlock.get(currentLevel) == 1):
+				var stars = _save.DivisionComplete.get(currentLevel)
+				loadStars(stars)
+			else:
+				self.disabled = true
 					
 			if(self.name == str(0)):
 				self.disabled = true
@@ -74,12 +61,11 @@ func checkStage():
 	levelTXt.text = text.capitalize()
 	
 
-	#load stars
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	mathType = PlayerVariables.stage
-
+func loadStars(stars):
+	if(stars != null):
+		for star in $stars.get_children():
+			if(int(star.name) <= stars):
+				star.show()
 
 func _on_Level_pressed():
 	SoundManager.play_se("Select")
